0fe981b00626debe8ed52001102a8fad1ad92e46
Redstoniker <287007098+Redstoniker@users.noreply.github.com>
2026-05-31 19:47:22 +0200

    Add Variable height, Better names

--- etchAsketch.js
+++ etchAsketch.js
@@ -1,22 +1,48 @@
 const container = document.querySelector(".container");
 let divs;
-let resetBoxesWithCount = sideLength => {
-    if (divs !== undefined) {
-        for( div of divs){
+let removeAllBoxes = () =>{
+    for(div of divs){
         container.removeChild(div);
-        }
     }
+}
 
-    for(let i = 0; i<sideLength**2; i++){
+let addBoxes = count => {
+     for(let i = 0; i<count; i++){
         container.appendChild(document.createElement("div"));
     }
-
-    divs = container.querySelectorAll(".container > div")
-
-    for (const [index,box] of divs.entries()) {
-    box.textContent = index;
-}
 }
 
-console.log(divs)
+let doWithBoxes = (method, ...params) => {
+    for (const [index, box] of divs.entries()) {
+        method(box, index, params);
+    }
+}
 
+let updateBoxList = () => divs = container.querySelectorAll(".container > div");
+
+let calculateBoxSize = sideLength => Math.floor(630/sideLength);
+
+let setBoxSize = (box,index,params) => {
+    let boxSize = params[0]+"px";
+    box.style.minWidth = boxSize;
+    box.style.maxHidth = boxSize;
+}
+changeContainerSize = (boxSize, sideLength) => {
+    container.style.Width = boxSize*sideLength+"px";
+    container.style.Height = boxSize*sideLength+"px";
+}
+
+let resetBoxesAndCount = sideLength => {
+    if (divs !== undefined) {
+        removeAllBoxes();
+    }
+    addBoxes(sideLength**2);    
+    updateBoxList();
+    doWithBoxes((box) => box.className = "box")
+    let boxSize = calculateBoxSize(sideLength);
+    doWithBoxes(setBoxSize, boxSize);
+
+
+}
+
+resetBoxesAndCount(100);
