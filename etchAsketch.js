c1d7decf86e500edcfa107fd29ecd695ade09cc1
Redstoniker <287007098+Redstoniker@users.noreply.github.com>
2026-06-23 00:43:30 +0200

    Add button js Buttons will now reset/clear the board. Main task complete

--- etchAsketch.js
+++ etchAsketch.js
@@ -1,6 +1,9 @@
 const container = document.querySelector(".container");
 const containerWidth = getComputedStyle(container).getPropertyValue("width");
 let boxes = document.querySelectorAll("#box");
+const resetButton = document.querySelector("#newB");
+const clearButton = document.querySelector("#clearB");
+let countPerSide = 16;
 
 const addNewBoxes = (count = 1, minwidth = "6px") => {
     for(let i = 0; i<count; i++) {
@@ -18,18 +21,18 @@ const deleteAllBoxes = () => {
     }
 }
 
-const calcBoxWidth = countPerSide => {
+const calcBoxWidth = () => {
     let containerWidthNumber = containerWidth.replace("px","");
     let boxWidth = containerWidthNumber / countPerSide;
     boxWidth += "px";
     return boxWidth;
 }
 
-const resetBoxes = countPerSide => {
+const resetBoxes = () => {
     if(0 < countPerSide && countPerSide <= 100){
         const totalBoxCount = countPerSide ** 2
         deleteAllBoxes();
-        addNewBoxes(totalBoxCount,calcBoxWidth(countPerSide));
+        addNewBoxes(totalBoxCount,calcBoxWidth());
         addAllBoxEventListeners();
     }
 }
@@ -42,4 +45,27 @@ function addAllBoxEventListeners () {
     }
 }
 
-resetBoxes(16);
+function clear (e) {
+    resetBoxes();
+    e.target.style.backgroundColor = "burlywood";
+    setTimeout(() => {
+        e.target.style.backgroundColor = "";
+    }, 100);
+}
+
+function newGrid (e) {
+    e.target.style.backgroundColor = "burlywood";
+    setTimeout(() => {
+        e.target.style.backgroundColor = "";
+    }, 100);
+    countPerSide=Number(prompt("How many boxes do you want per side?", "16"));
+    resetBoxes();
+    
+}
+
+
+
+resetBoxes();
+
+clearButton.addEventListener("click", clear);
+resetButton.addEventListener("click", newGrid)
